6c0b69de55f42d84c1eca0f18a001de5cdbeb13e
Fernanda135 <fernandacomun@gmail.com>
2025-03-19 14:12:39 -0300

    commit

--- 1.js
+++ 1.js
@@ -1,26 +1,26 @@
 import { Usuario, Publicacao, Comentario } from './1-class.js';
 
-const alice = new Usuario("Alice", "1990-05-15");
-const bob = new Usuario("Bob", "1992-08-20");
-const charlie = new Usuario("Charlie", "1988-12-30");
+const fernanda = new Usuario("Fernanda", "2006-10-22");
+const vitoria = new Usuario("Vitoria", "1996-10-22");
+const joao = new Usuario("Joao", "1988-12-30");
 
-const post1 = alice.criarPublicacao("Olá, mundo!", ["imagem1.jpg"]);
-const post2 = bob.criarPublicacao("Meu primeiro post!");
+const post1 = fernanda.criarPublicacao("Olá, mundo!", ["imagem1.jpg"]);
+const post2 = vitoria.criarPublicacao("Meu primeiro post!");
 
-bob.seguir(alice);
-charlie.seguir(alice);
+vitoria.seguir(fernanda);
+joao.seguir(fernanda);
 
-const comentario1 = new Comentario(bob, new Date(), "Adorei sua publicação!");
+const comentario1 = new Comentario(vitoria, new Date(), "Adorei sua publicação!");
 post1.comentar(comentario1);
 
 post2.curtir();
 post2.editar("Meu primeiro post editado!");
 
-const comentario2 = new Comentario(charlie, new Date(), "Muito legal, Alice!");
+const comentario2 = new Comentario(joao, new Date(), "Muito legal, Fernanda!");
 post1.comentar(comentario2);
 
-bob.deixarDeSeguir(alice);
+vitoria.deixarDeSeguir(fernanda);
 comentario1.remover(post1);
 
-console.log(`Publicação de ${alice.nome}: "${post1.descricao}" com ${post1.curtidas} curtidas e ${post1.comentarios.length} comentários.`);
-console.log(`Publicação de ${bob.nome}: "${post2.descricao}" com ${post2.curtidas} curtidas.`);
+console.log(`Publicação de ${fernanda.nome}: "${post1.descricao}" com ${post1.curtidas} curtidas e ${post1.comentarios.length} comentários.`);
+console.log(`Publicação de ${vitoria.nome}: "${post2.descricao}" com ${post2.curtidas} curtidas.`);
